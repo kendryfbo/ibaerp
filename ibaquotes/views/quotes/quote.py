e25fd5accb62967e91846b018c5f259e2770325e
kendry Finol <kfinol@burning-glass.com>
2021-02-28 20:23:33 -0300

    Fix order by group_num and item_num

--- ibaquotes/views/quotes/quote.py
+++ ibaquotes/views/quotes/quote.py
@@ -147,7 +147,7 @@ def quote_show(request,id):
 
     quote = Quote.objects.prefetch_related('quotedetail_set').get(pk=id)
     #groups = QuoteDetail.objects.filter(quote_id=id).order_by('group_num').distinct('group_num')
-    groups = QuoteDetail.objects.filter(quote_id=id).order_by(Coalesce('group_num','item_num').desc()).values('group_num','group_name','group_tax').annotate(group_subtotal=Sum('subtotal'))
+    groups = QuoteDetail.objects.filter(quote_id=id).order_by(Coalesce('group_num','item_num').asc()).values('group_num','group_name','group_tax').annotate(group_subtotal=Sum('subtotal'))
     quoteDetails = QuoteDetail.objects.filter(quote_id=id)
     quoteStatus = QuoteStatus.objects.all()
 
